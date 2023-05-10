e34f53c7e805bd96a6545a68cc5d1987d8511dae
sanieni6 <sanieni6@gmail.com>
2023-05-10 17:48:47 -0500

    Fix ruby linters

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-require_relative 'MyEnumerable'
+require_relative 'MyEnumerable.rb'
 
 # Create class MyList
 class MyList
@@ -7,10 +7,8 @@ class MyList
     @list = args
   end
 
-  def each
-    @list.each do |e|
-      yield e
-    end
+  def each(&block)
+    @list.each { |n| block.call(n) }
   end
 end
 
@@ -20,11 +18,11 @@ list = MyList.new(3, 4, 5, 6)
 # Test MyEnumerable methods
 all_numbers = list.all? { |n| n < 6 }
 any_three = list.any? { |n| n == 3 }
-even_numbers = list.filter { |n| n.even? }
+even_numbers = list.filter(&:even?)
 
 # Output
 puts <<~TEXT
-  All < 6: #{all_numbers}#{' '}
+  All < 6: #{all_numbers}
   Any 3: #{any_three}
   Even numbers found: #{even_numbers}
 TEXT
